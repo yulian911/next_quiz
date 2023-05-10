9270b5a7cfea793a884e62583a68411239071ec5
Yulian Melnychuk <julian95@o2.pl>
2023-05-10 14:23:01 +0200

    update

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,12 +4,12 @@ import './globals.css';
 import { Inter } from 'next/font/google';
 import useQuizStore from './store';
 
-const inter = Inter({ subsets: ['latin'] });
+// const inter = Inter({ subsets: ['latin'] });
 
-export const metadata = {
-  title: 'Quiz',
-  description: 'Quiz app',
-};
+// export const metadata = {
+//   title: 'Quiz',
+//   description: 'Quiz app',
+// };
 
 export default function RootLayout({
   children,
